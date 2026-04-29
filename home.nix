3b213a27640e35cbfd0cd9b77240a9942c38407f
Austin Bowman <astrangbow2@gmai.com>
2026-04-29 12:19:58 -0700

    added vicinae to home-manager

--- home.nix
+++ home.nix
@@ -7,6 +7,7 @@
     ./modules/neovim.nix
     ./modules/git.nix
     ./modules/yazi.nix
+    ./modules/vicinae.nix
   ];
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
