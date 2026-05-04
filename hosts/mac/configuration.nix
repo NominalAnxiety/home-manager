e964989e53f84673207d1c9bb23beb9218442c6c
Austin Bowman <astrangbow2@gmai.com>
2026-05-04 16:15:15 -0700

    added nix.enable = false to my mac configuration.nix

--- hosts/mac/configuration.nix
+++ hosts/mac/configuration.nix
@@ -4,6 +4,8 @@
   nixpkgs.hostPlatform = "aarch64-darwin";
   nixpkgs.config.allowUnfree = true;
 
+  nix.enable = false;
+
   users.users.austinbowman = {  # replace this with your actual username (find using whoami)
     name = "austinbowman"; # replace this with your actual username (find using whoami)
     home = "/Users/austinbowman"; # replace this with your actual username (find using whoami)
