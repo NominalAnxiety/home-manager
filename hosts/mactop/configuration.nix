146519ef04757e95682fc3cf53aaeb259a748738
Austin Bowman <astrangbow2@gmai.com>
2026-05-04 14:49:30 -0700

    added a mac host in home-manager

---
+++ hosts/mactop/configuration.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+{
+  system.stateVersion = 5;
+  nixpkgs.hostPlatform = "aarch64-darwin";
+
+  users.users.austinbowman = {  # replace this with your actual username (find using whoami)
+    name = "austinbowman"; # replace this with your actual username (find using whoami)
+    home = "/Users/austinbowman"; # replace this with your actual username (find using whoami)
+  };
+
+  nix.settings.experimental-features = [
+    "nix-flakes"
+    "nix-command"
+  ];
+  services.nix-daemon.enable = true;
+}
