dcfc733d6b63a1b0fafb3d00c327498012b93904
Austin Bowman <astrangbow2@gmai.com>
2026-05-08 13:22:25 -0700

    made nix manage treesitter parsers

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -1,27 +1,49 @@
-{ config, pkgs, inputs, ... }: {
+{
+  pkgs,
+  lib,
+  ...
+}:
+{
   programs.neovim = {
     enable = true;
     defaultEditor = true;
-    extraPackages = with pkgs; [
+
+    plugins = with pkgs.vimPlugins; [
+      lazy-nvim # nix manages lazy vim and lazyvim manages everything else
+      # nix managers tree-sitter autoinstall
+      (nvim-treesitter.withPlugins (p: [
+        p.lua
+        p.python
+        p.c
+        p.rust
+        p.cpp
+        p.nix
+      ]))
+    ];
+
+    initLua = ''require("config")'';
+    withRuby = false;
+    withPython3 = false;
+  };
+  home.packages =
+    with pkgs;
+    [
       # LSPs
       lua-language-server
       rust-analyzer
-	  ruff
-      clang-tools  # clangd
+      ruff
+      clang-tools # clangd
       nixd
       marksman
 
       # Formatters
       stylua
       rustfmt
-	  isort
+      isort
       black
-      clang-tools  # clang-format (same package as clangd)
-      nixfmt  # or nixfmt depending on your nixpkgs version
+      clang-tools # clang-format (same package as clangd)
+      nixfmt # or nixfmt depending on your nixpkgs version
       prettier
-
-	  # Linters
-
 
       # Treesitter
       tree-sitter
@@ -30,12 +52,6 @@
       # Telescope dependencies
       ripgrep
       fd
-
-    ] ++ lib.optionals stdenv.isLinux [ wl-clipboard ]; # adds wl-clipboard if on linux
-
-    plugins = with pkgs.vimPlugins; [ lazy-nvim ];
-    initLua = ''require("config")'';
-    withRuby = false;
-    withPython3 = false;
-  };
+    ]
+    ++ lib.optionals pkgs.stdenv.isLinux [ wl-clipboard ]; # adds wl-clipboard if on linux
 }
